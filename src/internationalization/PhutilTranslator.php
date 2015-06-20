104d0ae12d2186213a9cc0d0ce7b0fd24bea4ffe
wuxing <cyber4cn@gmail.com>
2015-06-21 01:35:03 +0800

    merge content

--- src/internationalization/PhutilTranslator.php
+++ src/internationalization/PhutilTranslator.php
@@ -136,13 +136,7 @@ final class PhutilTranslator extends Phobject {
       case 'en_W*':
       case 'en_R*':
       case 'en_A*':
-<<<<<<< HEAD
-<<<<<<< HEAD
       case 'zh_CN':
-=======
->>>>>>> 339837f3c6db741c545cef068ac63c842ce9bc28
-=======
->>>>>>> 339837f3c6db741c545cef068ac63c842ce9bc28
         list($singular, $plural) = $translations;
         if ($variant == 1) {
           return $singular;
